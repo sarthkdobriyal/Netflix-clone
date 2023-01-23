d7cebcef7c728c400676b7cef00989cfb578c310
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-01-23 16:32:29 +0530

    hmmm

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9KTZLjnPHbEayz63kxlIQzYPG2QLgtM4",
+    authDomain: "netflix-clone-cbc59.firebaseapp.com",
+    projectId: "netflix-clone-cbc59",
+    storageBucket: "netflix-clone-cbc59.appspot.com",
+    messagingSenderId: "992870822935",
+    appId: "1:992870822935:web:3d99bd0a63f1b45c609852"
+  };
