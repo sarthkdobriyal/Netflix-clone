2369d590c3c04f9a6946ffe82f68789b84a227f7
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-01-23 18:28:16 +0530

    banner styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,37 @@ import React from 'react'
 import "./Banner.css"
 
 function Banner() {
+
+    //to truncate the description if too long
+    function truncate(string, n){
+        return string.length > n?string.substring(0,n-1)+"...":string
+    }
+
+
   return (
-    <div>Banner</div>
+    <header className='banner' style={{
+        backgroundImage:`url("https://wallpapercave.com/wp/wp8741529.jpg")`,
+        backgroundSize:"cover",
+        backgroundPosition:"center center",
+    }}>
+
+        <div className="banner__contents">
+            <h1 className="banner__title">
+                Movie Name
+            </h1>
+            <div className="banner__buttons">
+                <button className='banner__button'>Play</button>
+                <button className='banner__button'>My List</button>
+            </div>
+            <h1 class="banner__description">{
+                truncate(`this is descriptionthis is descriptionthis is descriptionthis is descriptionthis is descriptionthis is `, 150)
+            }</h1>
+        </div>
+
+
+        <div className="banner--fadeBottom" />
+            
+    </header>
   )
 }
 
