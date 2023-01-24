c3e7bdeb1d4a33f3c9eac49441ecb8c929219c59
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-01-24 12:44:07 +0530

    yep

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import "./Plans.css"
 import db from '../firebase';
-import { collection, query, getDocs, where, getDoc,doc  } from "firebase/firestore"; 
+import { collection, query, getDocs, where } from "firebase/firestore"; 
 
 function Plans() {
 
     const [products , setProducts] = useState([]);
 
     useEffect(async() => {
+        //getting reference to the collection "products"
         const productsRef = collection(db, "products");
+        //creating query to get docs from product reference
         const q = query(productsRef, where("active", "==", true))
+        //getting all the docs from above query
         const querySnapshot = await getDocs(q);
+        //creating empty product object
         const products = {};
         querySnapshot.forEach(async(doc) => {
+            // for all the docs received storing the data
             products[doc.id] = doc.data();
+            //getting the prices collection in eact doc
             const priceSnap = await getDocs(collection(doc.ref,"prices"));
+            //storing the prices data for each product
             priceSnap.forEach(price => {
                 products[doc.id].prices = {
                     priceId: price.id,
@@ -22,6 +29,7 @@ function Plans() {
                 } 
             })        
         }); 
+        //finally setting the proucts state
         setProducts(products);
     }, [])
 
